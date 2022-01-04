b45066e71f7f3dc8df3a54d92663a5261afebd95
Kimmiich <kimmielundgren@live.se>
2022-01-04 17:48:27 +0100

    :sparkles: Added menu view mobile

--- script.js
+++ script.js
@@ -10,30 +10,4 @@ const scrollPostion = () => {
   };
 };
 
-const menubar = document.querySelector('.menuBtn');
-let burger = document.getElementById('BURGER-MENU');
-let lineTwo = document.getElementById('Line_2');
-let lineThree = document.getElementById('Line_3');
-const styling = 'transform: translate(0px, 27.267px)';
-
-menubar.addEventListener('click', () => {
-  console.log('Klick');
-});
-
-// let margin = 0;
-// const arrow = document.querySelector('.navArrow');
-// const products = document.querySelector('.productsWrapper');
-
-// arrow.addEventListener('click', () => {
-//   console.log('klick');
-//   margin -= 250;
-
-//   if (margin > -501) {
-//     products.style.marginLeft = margin + 'px';
-//     if (margin <= -500) {
-//       arrow.style.display = 'none';
-//     }
-//   }
-// });
-
 scrollPostion();
